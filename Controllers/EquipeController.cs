533cf053d74042b40ac2fdc0b9c017a77964b6d2
Dejailson Silva <kauakarate@gmail.com>
2020-07-15 16:23:59 -0300

    Upload de Imagem e Excluir feitos

--- Controllers/EquipeController.cs
+++ Controllers/EquipeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using E_PlayersProjeto.Models;
 using Microsoft.AspNetCore.Http;
+using System.IO;
 
 namespace E_PlayersProjeto.Controllers
 {
@@ -28,11 +29,44 @@ namespace E_PlayersProjeto.Controllers
             Equipe novaEquipe   = new Equipe();
             novaEquipe.IdEquipe = Int32.Parse(form["IdEquipe"]);
             novaEquipe.Nome     = form["Nome"];
-            novaEquipe.Imagem   = form["Imagem"];
+            // Upload Inicio
+            var file    = form.Files[0];
+            var folder  = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Equipes");
+
+            if(file != null)
+            {
+                if(!Directory.Exists(folder)){
+                    Directory.CreateDirectory(folder);
+                }
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/", folder, file.FileName);
+                using (var stream = new FileStream(path, FileMode.Create))  
+                {  
+                    file.CopyTo(stream);  
+                }
+                novaEquipe.Imagem   = file.FileName;
+            }
+            else
+            {
+                novaEquipe.Imagem   = "padrao.png";
+            }
+            // Upload Final
 
             equipeModel.Create(novaEquipe);            
             ViewBag.Equipes = equipeModel.ReadAll();
 
+            return LocalRedirect("~/Equipe");
+        }
+        /// <summary>
+        /// Método que ira excluir os objetos cadastraddos
+        /// </summary>
+        /// <param name="id">Id</param>
+        /// <returns></returns>
+        [Route("[controller]/{id}")]
+        public IActionResult Excluir(int id)
+        {
+            equipeModel.Delete(id);
+            ViewBag.Equipes = equipeModel.ReadAll();
             return LocalRedirect("~/Equipe");
         }
 
